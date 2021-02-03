26c46de23f23a10feea7b3564c6f00474e3fc4a9
Alexey <73654054+zinarous@users.noreply.github.com>
2021-02-03 23:12:21 +0500

    Calc_upd

--- calc_tk.py
+++ calc_tk.py
@@ -1,44 +1,51 @@
 from tkinter import *
 from tkinter import messagebox
-from tkinter import ttk
+from PIL import ImageTk, Image
 import math
 import sys
 
 root = Tk() 
-root.title("Calculator")
+root.title("Калькулятор")
 root.configure(bg="lightblue")
 root.resizable(width=False, height=False)
+root.geometry('310x300')
 
-bttn_list = [
-"7", "8", "9", "+", "*", 
-"4", "5", "6", "-", "/",
-"1", "2", "3",  "=", "xⁿ",
-"0", ".", "±",  "C",
-"Exit", "π", "sin", "cos",
-"(", ")","n!","√2", ]
+path = "Apps-Calc-icon.png"
+img = ImageTk.PhotoImage(Image.open(path))
+
+btns = [
+"7", "8", "9", "÷", "C", 
+"4", "5", "6", "×", "±",
+"1", "2", "3", "-", "xⁿ",
+"0", ".", "=", "+", "Exit",
+"e", "π", "sin", "cos",
+"n!", "(", ")","√2", ]
+
 
 r = 1
 c = 0
-for i in bttn_list:
+for i in btns:
     rel = ""
     cmd=lambda x=i: calc(x)
-    ttk.Button(root, text=i, command = cmd, width = 10 ).grid(row=r, column = c)
+    top_padding = 5
+    btn = Button(root, text=i, command = cmd, width = 7, height = 2, bg="white")
+    btn.grid(row = r, column = c, pady=2, padx = 1, sticky = E)
     c += 1
     if c > 4:
         c = 0
         r += 1
 
-calc_entry = Entry(root, width = 33)
-calc_entry.grid(row=0, column=0, columnspan=5)
+calc_entry = Entry(root, width = 50, justify = RIGHT)
+calc_entry.grid(row=0, column=0, columnspan=5, pady = 2, padx = 3)
 
 #логика калькулятора
 def calc(key):
     if key == "=":
 #исключение написания слов
-        str1 = "-+0123456789.*/)(" 
+        str1 = "-+0123456789.×÷)(" 
         if calc_entry.get()[0] not in str1:
             calc_entry.insert(END, "Это не выглядит как число, введи число!")
-            messagebox.showerror("АШИБКА", "Это не является числом!")
+            messagebox.showerror("ОШИБКА!!!", "Это не является числом!")
 #исчисления
         try:
             result = eval(calc_entry.get())
@@ -51,6 +58,12 @@ def calc(key):
     elif key == "C":
         calc_entry.delete(0, END)
         
+    elif key == "×":
+        calc_entry.insert(END, "*")
+
+    elif key == "÷":
+        calc_entry.insert(END, "/")    
+
     elif key == "±":
         if "=" in calc_entry.get():
             calc_entry.delete(0, END)
@@ -64,6 +77,9 @@ def calc(key):
 
     elif key == "π":
         calc_entry.insert(END, math.pi)
+
+    elif key == "e":
+        calc_entry.insert(END, math.e)
 
     elif key == "Exit":
         root.after(1,root.destroy)
